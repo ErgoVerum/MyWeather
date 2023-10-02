bf1dce7fe13bdfc35330ec68bed22a60ae707cc7
Julio Stohler <127553226+ErgoVerum@users.noreply.github.com>
2023-10-02 11:24:08 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import Pagina01 from './Pages/Pagina01'; 
+import Pagina02 from './Pages/Pagina02'; 
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Pagina02 />
+    </View>
+  );
+};
+
+export default App;
+
