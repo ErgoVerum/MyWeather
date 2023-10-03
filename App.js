2487e6561f0a8991a942a464a7864fd9006a80fd
Julio Stohler <127553226+ErgoVerum@users.noreply.github.com>
2023-10-03 11:18:41 +0000

    Update 4 files

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { View } from 'react-native';
-import Pagina01 from './Pages/Pagina01'; 
-import Pagina02 from './Pages/Pagina02'; 
+import React from "react";
+import { View } from "react-native";
+import Pagina01 from "./Pages/Pagina01";
+import Pagina02 from "./Pages/Pagina02";
 
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
-      <Pagina02 />
+      <Pagina01 />
     </View>
   );
 };
 
 export default App;
-
--- Pages/Pagina01.js
+++ Pages/Pagina01.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Image, Dimensions, Text, TouchableOpacity, TextInput } from 'react-native';
 
-
 const screenWidth = Dimensions.get('window').width; //Variavel para puxar a largura da tela. NÃO MEXER.
 
 const Pagina01 = () => {
